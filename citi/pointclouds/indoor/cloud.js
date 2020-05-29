02ff57f09ab9c77a8419097b35a347920de0549d
Arthur Alves Miyazaki <arthurm@lsi.usp.br>
2020-05-29 15:35:13 -0300

    3D model indoor

---
+++ citi/pointclouds/indoor/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 34812092,
+    "boundingBox": {
+        "lx": 323396.50800323489,
+        "ly": 7393806.240999222,
+        "lz": 0.1840234398841858,
+        "ux": 323501.97099876406,
+        "uy": 7393911.703994751,
+        "uz": 105.64701896905899
+    },
+    "tightBoundingBox": {
+        "lx": 323396.508,
+        "ly": 7393806.241,
+        "lz": 0.18402343999999999,
+        "ux": 323501.722,
+        "uy": 7393911.704,
+        "uz": 42.197023439999998
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.9133363366127014,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
