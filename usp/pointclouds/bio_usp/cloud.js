292d63a12e1a64f2659575ce08d24257b1d35023
Arthur Alves Miyazaki <arthurm@lsi.usp.br>
2020-08-29 20:05:02 -0300

    floresta point cloud

---
+++ usp/pointclouds/bio_usp/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 23302190,
+    "boundingBox": {
+        "lx": 323452.30099487307,
+        "ly": 7392577.607971191,
+        "lz": -36.0,
+        "ux": 324071.8790588379,
+        "uy": 7393197.186035156,
+        "uz": 583.5780639648438
+    },
+    "tightBoundingBox": {
+        "lx": 323452.301,
+        "ly": 7392577.608,
+        "lz": -36.0,
+        "ux": 323740.824,
+        "uy": 7393197.186,
+        "uz": 74.229
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 5.365703582763672,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
