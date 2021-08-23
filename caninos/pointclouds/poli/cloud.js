ae5455bb84ab80caf7a96002618f323d8876eae7
arthur miyazaki <arthurmiy@hotmail.com>
2021-08-23 12:50:10 -0300

    caninos page added

---
+++ caninos/pointclouds/poli/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 172742484,
+    "boundingBox": {
+        "lx": 322891.98680877688,
+        "ly": 7393467.294137955,
+        "lz": -0.0018786621093909162,
+        "ux": 323807.54997062686,
+        "uy": 7394382.857299805,
+        "uz": 915.5612831878661
+    },
+    "tightBoundingBox": {
+        "lx": 322891.987,
+        "ly": 7393467.294,
+        "lz": -0.00187866,
+        "ux": 323691.167,
+        "uy": 7394382.857,
+        "uz": 55.974121339999999
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 7.929009437561035,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ caninos/pointclouds/pr/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8240814,
+    "boundingBox": {
+        "lx": 323796.4949951172,
+        "ly": 7393239.119003296,
+        "lz": -0.03302490234375455,
+        "ux": 324325.87898254397,
+        "uy": 7393768.502990723,
+        "uz": 529.3509625244141
+    },
+    "tightBoundingBox": {
+        "lx": 323796.495,
+        "ly": 7393239.119,
+        "lz": -0.0330249,
+        "ux": 324285.111,
+        "uy": 7393768.503,
+        "uz": 56.3829751
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 4.58459997177124,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
