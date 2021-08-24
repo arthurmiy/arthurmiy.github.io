786a4de92a1881279413e52082c320c8c1f33339
arthur miyazaki <arthurmiy@hotmail.com>
2021-08-24 09:22:03 -0300

    map error correction assets and css added

---
+++ erasmos/pointclouds/erasmos/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 141022664,
+    "boundingBox": {
+        "lx": 160.8800048828125,
+        "ly": 139.08999633789063,
+        "lz": -808.2000122070313,
+        "ux": 279.7799987792969,
+        "uy": 257.989990234375,
+        "uz": -689.3000183105469
+    },
+    "tightBoundingBox": {
+        "lx": 160.88,
+        "ly": 139.09,
+        "lz": -808.2,
+        "ux": 277.12,
+        "uy": 257.99,
+        "uz": -775.98
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.0297040939331055,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
