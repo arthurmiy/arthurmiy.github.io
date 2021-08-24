257fe70dde7dcb580734bcb83e95aaa0fe15f335
arthur miyazaki <arthurmiy@hotmail.com>
2021-08-24 09:54:52 -0300

    jaguariuna added, index page

---
+++ jaguariuna/pointclouds/se/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 31833114,
+    "boundingBox": {
+        "lx": 294633.5591430664,
+        "ly": 7487999.111602783,
+        "lz": 552.7294921875,
+        "ux": 295156.2628631592,
+        "uy": 7488521.815322876,
+        "uz": 1075.4332122802734
+    },
+    "tightBoundingBox": {
+        "lx": 294633.559,
+        "ly": 7487999.112,
+        "lz": 552.729,
+        "ux": 295066.31,
+        "uy": 7488521.815,
+        "uz": 663.908
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.526747226715088,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
